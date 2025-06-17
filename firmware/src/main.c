8089fd0e6000a07a7a010246274edfe443a4f736
olell <olel@olel.de>
2025-06-17 23:03:55 +0200

    ✨ enabled animation cycling

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -6,9 +6,18 @@
 #include "animation.h"
 #include "animations/animationPlayer.h"
 #include "animations/ledvm.h"
+#include "animations/cycle.h"
+#include "animations/droplets.h"
+#include "animations/firework.h"
+#include "animations/ledvm.h"
+#include "animations/matrix.h"
+#include "animations/pac.h"
+#include "animations/rolling_text.h"
 #include "ch32fun.h"
 #include "charlie.h"
 #include "systick.h"
+
+#define ANIMATION_CYCLE_INTERVAL 20000 // milliseconds between each animation change
 
 static const animation_t* animations[MAX_ANIMATIONS];
 static size_t animation_count = 0;
@@ -35,12 +44,12 @@ int main() {
 
     // register animations here
     register_animation(&ledvm_animation);
-    // register_animation(&cycle_animation);
-    // register_animation(&pac_animation);
-    // register_animation(&animation_player);
-    // register_animation(&rolling_text_animation);
-    // register_animation(&firework_animation);
-    // register_animation(&matrix_animation);
+    register_animation(&cycle_animation);
+    register_animation(&pac_animation);
+    register_animation(&animation_player);
+    register_animation(&rolling_text_animation);
+    register_animation(&firework_animation);
+    register_animation(&matrix_animation);
 
     current_animation_idx = 0;
     current_animation = animations[current_animation_idx];
@@ -48,17 +57,17 @@ int main() {
 
     charlieSetup();
 
-    // uint32_t anim_start_time = millis();
+    uint32_t anim_start_time = millis();
 
     for (;;) {
         current_animation->tick();
         Delay_Ms(current_animation->tick_interval);
-        // if (millis() - anim_start_time > 2000) {
-        //     anim_start_time = millis();
-        //     if (++current_animation_idx >= animation_count)
-        //         current_animation_idx = 0;
-        //     current_animation = animations[current_animation_idx];
-        //     current_animation->init();
-        // }
+        if (millis() - anim_start_time > ANIMATION_CYCLE_INTERVAL) {
+            anim_start_time = millis();
+            if (++current_animation_idx >= animation_count)
+                current_animation_idx = 0;
+            current_animation = animations[current_animation_idx];
+            current_animation->init();
+        }
     }
 }
